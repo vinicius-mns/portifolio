c99f6bf73e4d212ac55ddd09aba8bce06ffbc9e0
vinicius silva <vinicius_dev@hotmail.com>
2023-03-27 00:33:56 -0300

    feat: hash router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ViniciusT from '../components/ViniciusT.vue'
 
 const router = createRouter({
